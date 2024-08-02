333e3dff3c7957fccc25e3862a8360aee13d2aba
Niraj-Dvio <nirajchordia@dviodigital.com>
2024-08-02 18:35:31 +0530

    p1 improve:Connector card

--- resources/js/Components/ConnectCardButton.vue
+++ resources/js/Components/ConnectCardButton.vue
@@ -12,30 +12,64 @@
                 class="h-4 w-auto"
             />
         </div>
-        <button @click="connectAccount" class="rounded-lg p-2">
+        <button @click="createConnector" class="rounded-lg p-2">
             Connect your {{ props.name }} account
         </button>
+        <div v-if="loading"><Loader /></div>
     </div>
 </template>
 <script setup>
+import { ref } from "vue";
 import axios from "../utils/axiosInstance";
-const props = defineProps(["connectorId", "name"]);
-console.log(props.connectorId);
+import Loader from "./Loader.vue";
+const connectorId = ref("");
+const props = defineProps(["name"]);
 const data = {
     connect_card_config: {
         redirect_uri: "http://localhost/dashboard",
     },
 };
+const service = ref();
+const loading = ref(false);
+switch (props.name) {
+    case "Google Ads":
+        service.value = "google_ads";
+        break;
+    case "Google Analytics":
+        service.value = "google_analytics_4";
+        break;
+    case "Facebook Ads":
+        service.value = "facebook_ads";
+        break;
+    default:
+        service.value = "";
+        break;
+}
+
+const payload = {
+    service: service.value,
+    group_id: "reverent_description",
+    config: {
+        schema: `${service.value}_1`,
+    },
+};
+const createConnector = async () => {
+    loading.value = true;
+    const response = await axios.post("/connectors", payload);
+    connectorId.value = response.data?.data.id;
+    if (connectorId.value !== "") {
+        connectAccount();
+    }
+};
 const connectAccount = async () => {
     const response = await axios.post(
-        `/connectors/${props.connectorId}/connect-card`,
+        `/connectors/${connectorId.value}/connect-card`,
         data
     );
-    console.log(response && response.data?.data.connect_card);
+    loading.value = true;
     const uri = response.data?.data.connect_card.uri;
-    console.log(uri);
-    // if (uri) {
-    //     window.location.href = uri;
-    // }
+    if (uri) {
+        window.location.href = uri;
+    }
 };
 </script>
--- resources/js/Components/ConnectorCard.vue
+++ resources/js/Components/ConnectorCard.vue
@@ -1,17 +1,10 @@
 <template>
     <div class="flex flex-col mx-auto p-3">
-        <ConnectCardButton :connectorId="googleAdsID" name="Google Ads" />
-        <ConnectCardButton :connectorId="facebookAdsID" name="Facebook Ads" />
-        <ConnectCardButton
-            :connectorId="googleAnalyticsID"
-            name="Google Analytics"
-        />
+        <ConnectCardButton name="Google Ads" />
+        <ConnectCardButton name="Facebook Ads" />
+        <ConnectCardButton name="Google Analytics" />
     </div>
 </template>
 <script setup>
 import ConnectCardButton from "./ConnectCardButton.vue";
-const googleAdsID = import.meta.env.VITE_FIVETRAN_GOOGLE_ADS_CONNECTOR_ID;
-const facebookAdsID = import.meta.env.VITE_FIVETRAN_FACEBOOK_ADS_CONNECTOR_ID;
-const googleAnalyticsID = import.meta.env
-    .VITE_FIVETRAN_GOOGLE_ANALYTICS_CONNECTOR_ID;
 </script>
